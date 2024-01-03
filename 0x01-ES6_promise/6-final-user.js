71aeb806456feb49d8fbf4a6d83c8ff6f5d77572
washucode <estherw8525@gmail.com>
2024-01-04 01:08:37 +0300

    task 6 corrections

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,6 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 /* eslint-enable */
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName = '', lastName = '', filename = '') {
   return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
 }
