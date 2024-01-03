eeb2edbc37d0427b2d1acced421cb7117669aae5
washucode <estherw8525@gmail.com>
2024-01-04 01:05:58 +0300

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+/* eslint-enable */
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
+}
