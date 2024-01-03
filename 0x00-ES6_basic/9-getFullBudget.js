21f776eae53e2e7e2b4c8dd50a2c9e57534e689a
washucode <estherw8525@gmail.com>
2024-01-03 20:45:57 +0300

    task 9

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,14 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    /* eslint-disable */
+    getIncomeInDollars: (income) => `$${income}`,
+    /* eslint-disable */
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
