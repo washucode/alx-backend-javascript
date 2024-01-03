20721416dce60a7c8bfb069a014138c2b0ada50f
washucode <estherw8525@gmail.com>
2024-01-04 00:10:23 +0300

    task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+    (success) ? resolve({
+      status: 200,
+      body: 'Success',
+    }) : reject(
+      new Error('The fake API is not working currently')
+    );
+    /* eslint-enable */
+  });
+}
