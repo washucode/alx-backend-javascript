5ace189ac05ebcb3d53755a8e57b4b34a3ecd7ca
washucode <estherw8525@gmail.com>
2024-01-08 21:13:46 +0300

    task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
+
+export default Car;
