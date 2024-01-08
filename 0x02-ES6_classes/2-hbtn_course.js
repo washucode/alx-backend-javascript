9ccd73a5bbda4a2fc3410e2ab5d801197080ec30
washucode <estherw8525@gmail.com>
2024-01-08 19:51:43 +0300

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+/*
+* Create a class named HolbertonCourse that defines a Holberton course:
+*/
+
+export default class HolbertonCourse {
+  /**
+   *
+   * @param {string} name
+   * @param {Number length
+   * @param {String} students
+   */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (value.every((student) => typeof student === 'string') === false) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._students = value;
+  }
+}
