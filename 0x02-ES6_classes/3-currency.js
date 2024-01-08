94ce94668e9fefa4055c40ae1e13827789b44994
washucode <estherw8525@gmail.com>
2024-01-08 20:05:37 +0300

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,43 @@
+/**
+ * Currency Class Implementation
+ */
+
+class Currency {
+  /**
+   *
+   * @param {string} code
+   * @param {string} name
+   */
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+export default Currency;
